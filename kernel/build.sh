685d864a99d790d0ecd5074321a3d9d198933174
Yuya Hamamachi <yuya.hamamachi.sx@renesas.com>
2026-02-15 00:52:40 +0900

    Add support to build kernel
    Signed-off-by: Yuya Hamamachi <yuya.hamamachi.sx@renesas.com>

---
+++ kernel/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+RAW_VERSION=${1:-6.12.66-2026-01-19}
+VERSION=$(echo $RAW_VERSION | cut -d'-' -f1)
+
+PKG=sparrow-hawk-kernel-6.12
+wget -O ${PKG}_${VERSION}.orig.tar.gz \
+    -c https://github.com/rcar-community/linux/archive/refs/heads/renesas-lts/v${RAW_VERSION}-sparrow-hawk.tar.gz
+
+rm -rf ${PKG}-${VERSION}
+mkdir ${PKG}-${VERSION}
+tar xf ${PKG}_${VERSION}.orig.tar.gz --strip-components=1 -C ${PKG}-${VERSION}
+
+cd ${PKG}-${VERSION}
+cp -r ../debian .
+
+dpkg-buildpackage -us -uc
+
