1cacb9100aab48b5ddb2f29d66085945e0c69a78
Yuya Hamamachi <yuya.hamamachi.sx@renesas.com>
2026-03-05 02:03:35 +0900

    Add native container for cross building
    Signed-off-by: Yuya Hamamachi <yuya.hamamachi.sx@renesas.com>

--- build_docker_image.sh
+++ build_docker_image.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-docker build --platform linux/arm64 -t debian-arm64-builder .
+docker build --platform linux/arm64 -t debian-arm64-builder ./_dev/arm64
+docker build --platform linux/amd64 -t debian-amd64-builder ./_dev/x64
 
