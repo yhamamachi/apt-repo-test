f35dfc22d12d5594b227689de1dcca1475f14ff8
Yuya Hamamachi <yuya.hamamachi.sx@renesas.com>
2026-03-04 23:40:26 +0900

    atf: build.sh: Change to use docker container
    Signed-off-by: Yuya Hamamachi <yuya.hamamachi.sx@renesas.com>

--- atf/build.sh
+++ atf/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-VERSION=$1
+SCRIPT_DIR=$(cd `dirname $0` && pwd)
+VERSION=${1:-2.14.0}
 PKG=sparrow-hawk-arm-trusted-firmware
 wget -O ${PKG}_${VERSION}.orig.tar.gz \
     -c https://github.com/ARM-software/arm-trusted-firmware/archive/refs/tags/v${VERSION}.tar.gz 
@@ -8,8 +9,11 @@ wget -O ${PKG}_${VERSION}.orig.tar.gz \
 rm -rf ${PKG}-${VERSION}
 mkdir ${PKG}-${VERSION}
 tar xf ${PKG}_${VERSION}.orig.tar.gz --strip-components=1 -C ${PKG}-${VERSION}
-cd ${PKG}-${VERSION}
+cp -r ${SCRIPT_DIR}/debian -t ${PKG}-${VERSION}
 
-cp -r ../debian .
-dpkg-buildpackage -us -uc
+docker run --rm -it --platform linux/arm64 \
+    -v ${SCRIPT_DIR}:/build:Z -u $(id -u):$(id -g) \
+    -w /build/${PKG}-${VERSION} \
+    debian-arm64-builder \
+    dpkg-buildpackage -us -uc
 
