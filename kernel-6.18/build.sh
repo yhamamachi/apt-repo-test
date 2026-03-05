e3fddf0a63345c35ea4a85f13a78279a080f2492
Yuya Hamamachi <yuya.hamamachi.sx@renesas.com>
2026-03-05 15:46:23 +0900

    kernel-6.18: build.sh: Change to use git clone instead of wget
    Signed-off-by: Yuya Hamamachi <yuya.hamamachi.sx@renesas.com>

--- kernel-6.18/build.sh
+++ kernel-6.18/build.sh
@@ -2,15 +2,21 @@
 
 SCRIPT_DIR=$(cd `dirname $0` && pwd)
 RAW_VERSION=${1:-6.18.6-2026-01-20}
+BRANCH=origin/renesas-lts/v${RAW_VERSION}-sparrow-hawk
 VERSION=$(echo $RAW_VERSION | cut -d'-' -f1)
-
 PKG=sparrow-hawk-kernel-6.18
-wget -O ${PKG}_${VERSION}.orig.tar.gz \
-    -c https://github.com/rcar-community/linux/archive/refs/heads/renesas-lts/v${RAW_VERSION}-sparrow-hawk.tar.gz
 
+cd ${SCRIPT_DIR}
+git clone https://github.com/rcar-community/linux.git
+cd linux
+git fetch
+git archive ${BRANCH} -o ../${PKG}_${VERSION}.orig.tar.gz
+
+
+cd ${SCRIPT_DIR}
 rm -rf ${PKG}-${VERSION}
 mkdir ${PKG}-${VERSION}
-tar xf ${PKG}_${VERSION}.orig.tar.gz --strip-components=1 -C ${PKG}-${VERSION}
+tar xf ${PKG}_${VERSION}.orig.tar.gz --strip-components=0 -C ${PKG}-${VERSION}
 
 cp -r ${SCRIPT_DIR}/debian -t ${PKG}-${VERSION}
 
