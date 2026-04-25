fb3ba19d92fa27cb15d50a20deb2295d54640bff
Yuya Hamamachi <yuya.hamamachi.sx@renesas.com>
2026-04-25 13:25:44 +0900

    _dev: Add debian12 build envrionment
    Signed-off-by: Yuya Hamamachi <yuya.hamamachi.sx@renesas.com>

--- build_docker_image.sh
+++ build_docker_image.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-docker build --platform linux/arm64 -t debian-arm64-builder ./_dev/arm64
+docker build --platform linux/arm64 -t debian-12-arm64-builder ./_dev/arm64/debian12
+docker build --platform linux/arm64 -t debian-13-arm64-builder ./_dev/arm64/debian13
 docker build --platform linux/amd64 -t debian-amd64-builder ./_dev/x64
 
