cb6b06a2ca2cd841f4ea747f7e0bebff2fdaf316
Yuya Hamamachi <yuya.hamamachi.sx@renesas.com>
2026-04-25 13:43:37 +0900

    libcamera: build.sh: Add support multiple distribution
    Signed-off-by: Yuya Hamamachi <yuya.hamamachi.sx@renesas.com>

--- libcamera/build.sh
+++ libcamera/build.sh
@@ -2,8 +2,24 @@
 
 SCRIPT_DIR=$(cd `dirname $0` && pwd)
 COMMIT=f4c3dee21770b9b8817c80265b9f81eda1833731
-VERSION=${1:-0.6.0}
+VERSION=0.6.0
 PKG=sparrow-hawk-libcamera
+DEBIAN_VER=""
+# Check version and codename
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        --version)
+            DEBIAN_VER=$2
+            shift ;;
+        *) ;; # Ignore unknown option
+    esac
+    shift
+done
+if [[ "${DEBIAN_VER}" == "" ]];then
+    echo "Error: Target debian version is not specified"
+    echo " Ex.) $0 --version 13"
+    exit -1
+fi
 
 cd ${SCRIPT_DIR}
 git clone https://git.libcamera.org/libcamera/libcamera.git
@@ -17,10 +33,12 @@ mkdir ${PKG}-${VERSION}
 tar xf ${PKG}_${VERSION}.orig.tar.gz --strip-components=0 -C ${PKG}-${VERSION}
 
 cp -r ${SCRIPT_DIR}/debian -t ${PKG}-${VERSION}
+sed -i ${PKG}-${VERSION}/debian/changelog \
+    -e "s/\(${VERSION}-[0-9]*\)/\1+deb${DEBIAN_VER}/"
 
 docker run --rm -it --platform linux/arm64 \
     -v ${SCRIPT_DIR}:/build:Z -u $(id -u):$(id -g) \
     -w /build/${PKG}-${VERSION} \
-    debian-arm64-builder \
+    debian-${DEBIAN_VER}-arm64-builder \
     dpkg-buildpackage -us -uc -a arm64
 
