30a218efb2a2502f42ea15f678243b73cbae852f
Yuya Hamamachi <yuya.hamamachi.sx@renesas.com>
2026-05-21 11:07:50 +0900

    build_docker_image.sh: Fix execution path limitation
    Signed-off-by: Yuya Hamamachi <yuya.hamamachi.sx@renesas.com>

--- build_docker_image.sh
+++ build_docker_image.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+SCRIPT_DIR=$(cd `dirname $0` && pwd)
+cd $SCRIPT_DIR
+
 docker build --platform linux/arm64 -t debian-12-arm64-builder ./_dev/arm64/debian12
 docker build --platform linux/arm64 -t debian-13-arm64-builder ./_dev/arm64/debian13
 docker build --platform linux/amd64 -t debian-amd64-builder ./_dev/x64
